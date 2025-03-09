96d3f7ab10b39815d65137e844f298ecf3362768
Szejmous <arekstachyra97@gmail.com>
2025-03-09 21:30:09 +0100

    Add files via upload

--- chat.py
+++ chat.py
@@ -12,8 +12,8 @@ computer_name = socket.gethostname()
 user_names = {
     "BIMES_3": "Mateusz",
     "BIMES_11": "Arek",
-    "BIMES_2": "Natalia"
-"KNKPK": "Arek Dom"
+    "BIMES_2": "Natalia",
+    "KNKPK": "Arek Dom"
 }
 
 # Przypisz nazwę użytkownika, jeśli jest w mapie, inaczej użyj nazwy komputera
